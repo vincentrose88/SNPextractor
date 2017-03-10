b6d47406b8feeef1020b73d61c9ab0da6c6d6a9a
Vincent Appel <cxt155@porus01.sund.root.ku.dk>
2017-03-10 11:15:16 +0100

    updated so the all.SNPs.vcf is not gzipped and have dummy names for missing names

--- sub_scripts/convertToGRS.sh
+++ sub_scripts/convertToGRS.sh
@@ -1,3 +1,3 @@
-cp $1/all.SNPs.extracted.vcf.gz all.SNPs.vcf
+zcat $1/all.SNPs.extracted.vcf.gz | sed -r 's/([0-9]+)\t([0-9]+)\t\./\1\t\2\tchr\1\:\2/g' > all.SNPs.vcf
 sed -r 's/([0-9]+)\t([0-9]+)\t\./\1\t\2\tchr\1\:\2/g' $1/genoFile.noHead > genoFile.noHead
 mv $1/newHeader .
